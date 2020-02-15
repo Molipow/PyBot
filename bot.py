bbc24f081c14b085e6a14a47e77f06b8154152c2
Molipow <molipowgamine@gmail.com>
2020-02-15 16:58:13 +0100

    remindme command added

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import discord
-import random
-import math
 import os
-from discord.ext import commands
+import json
+import time
+from discord.ext import commands, tasks
 
 # https://discordapp.com/oauth2/authorize?client_id=677895248088662025&permissions=8&scope=bot              Add bot with Admin permission
 # https://discordapp.com/api/oauth2/authorize?client_id=677895248088662025&permissions=518208&scope=bot     Add bot with Non-admin permissions
@@ -11,6 +11,7 @@ client = commands.Bot(command_prefix = '!py ')
 
 @client.event
 async def on_ready():
+    check_reminds.start()
     print("Bot working")
     await client.change_presence(status=discord.Status.online, activity=discord.Game("!py pomoc"))
 
@@ -22,17 +23,52 @@ async def on_member_join(member):
 async def on_member_remove(member):
     print(f'{member} wyszedł')
 
+@client.command(aliases=["reset","restart", "reboot"])
+@commands.has_permissions(administrator = True)
+async  def _reset(ctx):
+    await ctx.send("Restartowanie bota")
+    await client.close()
+    os.system("cls")
+    os.system("echo Bot restarting")
+    os.system("python bot.py")
+
+@tasks.loop(seconds = 1)
+async def check_reminds():
+    to_pop=[]
+    with open('remind.json', 'r') as f:
+        reminds = json.load(f)
+    for item in reminds:
+        if float(item) <= float(time.time()):
+            channel = client.get_channel(int(reminds[item][1]))
+            author = client.get_user(int(reminds[item][2]))
+            message = str(reminds[item][0])
+            to_pop.append(item)
+            await channel.send(f"RemindMe! przez {author.mention}. Wiadomość: {message}")
+    for i in to_pop:
+        reminds.pop(i)
+    with open('remind.json', 'w') as f:
+        json.dump(reminds, f, indent=4)
+
+@check_reminds.after_loop
+async def check_reminds_restart():
+    print('done!')
+    print(check_reminds.failed())
+    check_reminds.restart()
+
 @client.command()
+@commands.has_permissions(administrator = True)
 async def load(ctx, extension):
     client.load_extension(f"cogs.{extension}")
-    await ctx.send(f"Załadowano moduł {extension}")
+    await ctx.send(f"Włączono moduł {extension}")
 
 @client.command()
+@commands.has_permissions(administrator = True)
 async def unload(ctx, extension):
     client.unload_extension(f"cogs.{extension}")
-    await ctx.send(f"Rozładowano moduł {extension}")
+    await ctx.send(f"Wyłączono moduł {extension}")
 
 @client.command()
+@commands.has_permissions(administrator = True)
 async def reload(ctx, extension):
     client.unload_extension(f"cogs.{extension}")
     client.load_extension(f"cogs.{extension}")
--- cogs/utilities.py
+++ cogs/utilities.py
@@ -1,6 +1,8 @@
 import discord
 import random
 import math
+import time
+import json
 from discord.ext import commands
 
 class Utilities(commands.Cog):
@@ -44,21 +46,24 @@ class Utilities(commands.Cog):
         await ctx.send(f"```Wyrzuciłeś {result}. {response}```")
         
     @commands.command()
+    @commands.has_permissions(manage_messages = True)
     async def czyść(self, ctx, amount = 5):
         await ctx.channel.purge(limit = amount+1)
         
     @commands.command()
     async def pomoc(self, ctx, command_for_help = None):
         if command_for_help == None:
-            await ctx.send("```Komendy:\n  czyść\n  ping\n  pong\n  pomoc\n  rzuć\n  operator\n  syntax\nNapisz `!py pomoc [komenda]` aby dostać dokładniejsze informacje.```")
+            await ctx.send("```Komendy:\n  czyść\n  ping\n  pong\n  pomoc\n  remindme\n  rzuć\n  operator\n  syntax\nNapisz `!py pomoc [komenda]` aby dostać dokładniejsze informacje.```")
         elif command_for_help == "czyść":
-            await ctx.send("```Komenda czyść\n  czyść [liczba, domyślnie 5]\nCzyści n wiadomości z czatu.```")
+            await ctx.send("```Komenda czyść\n  czyść <liczba>\nCzyści n(domyślnie 5) wiadomości z czatu.```")
         elif command_for_help == "ping":
             await ctx.send("```Komenda ping\n  ping\nPong!```")
         elif command_for_help == "pong":
             await ctx.send("```Komenda pong\n  pong\nPing!```")
         elif command_for_help == "pomoc":
             await ctx.send("```Komenda pomoc\n  pomoc\nWyświetla listę komend.```")
+        elif command_for_help == "remindme":
+            await ctx.send("```Komenda remindme\n  remindme [czas] [wiadomość] <kanał>\nUstawia przypomnienie wraz z wiadomością. Jeśli wiadomość zawiera więcej niż jedno słowo, nalezy ją umieścić w cudzysłowie. Jeśli kanał nie zostanie wybrany, przypomnienie zostanie wysłane na kanale z którego została wykonana komenda.```")
         elif command_for_help == "rzuć":
             await ctx.send("```Komenda rzuć\n  rzuć [liczba]\nRzuca kostką n-ścienną.```")
         elif command_for_help == "operator":
@@ -68,5 +73,43 @@ class Utilities(commands.Cog):
         else:
             await ctx.send("```Nie znaleziono komendy.```")
 
+    @commands.command()
+    async def github(self, ctx):
+        await ctx.send("https://github.com/Molipow/PyBot")
+
+    @commands.command()
+    async def remindme(self, ctx, remind_time, message, channel = None):
+        unit = remind_time[-1:]
+        time_to_add_raw = int(remind_time[:-1])
+        if unit == 's' or unit == 'S':
+            time_to_add = time_to_add_raw
+        elif unit == 'm' or unit == 'M':
+            time_to_add = time_to_add_raw * 60
+        elif unit == 'h' or unit == 'H':
+            time_to_add = time_to_add_raw * 60 * 60
+        elif unit == 'd' or unit == 'D':
+            time_to_add = time_to_add_raw * 60 * 60 * 24
+        else:
+            time_to_add = time_to_add_raw
+            
+        remind_date = time.time() + time_to_add
+
+        with open("remind.json", "r") as f:
+            reminds = json.load(f)
+
+        if channel == None:
+            channel = ctx.channel
+        else:
+            channel = channel[2:]
+            channel = channel[:-1]
+            channel = self.client.get_channel(int(channel))
+            
+        reminds[str(remind_date)] = message, str(channel.id), str(ctx.author.id)
+
+        with open("remind.json", "w") as f:
+            json.dump(reminds, f, indent=4)
+
+        await ctx.send(f"Przypomne za {remind_time} na kanale {channel.mention}. Wiadomość: {message}")
+    
 def setup(client):
     client.add_cog(Utilities(client))
